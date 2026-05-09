dc4f44adf9dfd5300886b3344d17e283e5caeaf9
Nur <xzqb77@gmail.com>
2026-05-09 12:34:47 +0600

    Add new class Game

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    private List<task> cats = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+
+
+}
