d241e49f9ea4a8025c8626c62b48db31ab61fe1f
Nur <xzqb77@gmail.com>
2026-05-09 11:50:54 +0600

    Add new class

---
+++ src/task.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class task {
+    private String name;
+    private int age;
+    private int health;
+    private int mood;
+    private int satiety;
+    private boolean actionDoneToday = false;
+
+    public task(String name, int age) {
+        Random rand = new Random();
+        this.name = name;
+        this.age = age;
+        this.health = rand.nextInt(61) + 20;
+        this.mood = rand.nextInt(61) + 20;
+        this.satiety = rand.nextInt(61) + 20;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public boolean isActionDoneToday() {
+        return actionDoneToday;
+    }
+
+    public void setActionDoneToday(boolean state) {
+        this.actionDoneToday = state;
+    }
+
+
+}
